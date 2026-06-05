c4166d8c5bf8d36735eda015e10396a8804f1e69
Desert <70640792+TheDesert476@users.noreply.github.com>
2026-06-05 19:44:07 +0000

    development.rb fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.enable_reloading = true
   config.eager_load = false
+  config.hosts = [
+    "localhost",
+    /.*\.app\.github\.dev/
+  ]
   config.consider_all_requests_local = true
   config.server_timing = true
   config.action_controller.perform_caching = false
